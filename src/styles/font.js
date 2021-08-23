70f4ed9eea09a429a486cbbcea7ae17ab495871b
jollyjunkaih <jwong091@e.ntu.edu.sg>
2021-08-23 10:03:59 +0800

    changes

--- src/styles/font.js
+++ src/styles/font.js
@@ -1,11 +1,26 @@
 import styled from "@emotion/styled";
 
-export const CopyRightText = styled.p`
-  color: "#B0B0B0";
-`;
+// export const CopyRightText = styled.p`
+//   font-size: 1vw;
+//   color: "#B0B0B0";
+// `;
 
-export const FollowUsText = styled.p`
-  font-size: 2vw;
-  color: "#8EAB3D";
-  font-weight: bold;
-`;
+export const CopyRightText = ({ children }) => {
+  return <p style={{ fontSize: "1vw", color: "#B0B0B0" }}>{children}</p>;
+};
+
+//Can do a if else for phone or desktop if ... return ... else, return ...
+
+export const FollowUsText = ({ children }) => {
+  return (
+    <p style={{ fontSize: "2vw", color: "black", fontWeight: "bold" }}>
+      {children}
+    </p>
+  );
+};
+
+// export const FollowUsText = styled.p`
+//   font-size: 2vw;
+//   color: "#8EAB3D";
+//   font-weight: bold;
+// `;
